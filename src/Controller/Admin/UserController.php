7ab0d209c8cbeaf4586eda6fbfc1a3cdcad91f31
baktybekovich- <kydyrmysheva2005@gmail.com>
2022-08-23 14:20:56 +0600

    authorization

---
+++ src/Controller/Admin/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/users', name: 'app_admin')]
+    public function index(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+        return $this->render(view: 'admin/index.html.twig', parameters: [
+            'users' => $users
+        ]);
+    }
+
+    #[Route('/users/add/{id}', name: 'app_admin_add')]
+    public function add(UserRepository $userRepository, Request $request, \Doctrine\Persistence\ManagerRegistry $managerRegistry): Response
+    {
+        $user = $userRepository->find($request->get('id'));
+        $user->setRoles(['ROLE_ADMIN']);
+        $managerRegistry->getManager()->flush();
+        return $this->redirectToRoute('app_admin');
+    }
+
+    #[Route('/users/lower/{id}', name: 'app_admin_lower')]
+    public function lower(UserRepository $userRepository, Request $request, \Doctrine\Persistence\ManagerRegistry $managerRegistry): Response
+    {
+        $user = $userRepository->find($request->get('id'));
+        $user->setRoles(['ROLE_USER']);
+        $managerRegistry->getManager()->flush();
+        return $this->redirectToRoute('app_admin');
+    }
+
+    #[Route('/users/delete/{id}', name: 'app_user_delete')]
+    public function delete(UserRepository $userRepository, Request $request, \Doctrine\Persistence\ManagerRegistry $managerRegistry): Response
+    {
+        $user = $userRepository->find($request->get('id'));
+        $userRepository->remove($user);
+        $managerRegistry->getManager()->flush();
+        return $this->redirectToRoute('app_admin');
+    }
+
+
+}
